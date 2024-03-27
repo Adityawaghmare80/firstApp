83ceb001ae29b8805bdfd050a327fcb6d028b316
Adityawaghmare80 <148642692+Adityawaghmare80@users.noreply.github.com>
2024-03-26 18:48:43 -0700

    angular table and list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iobject } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstApp';
+  skills: string[] = ["HTML", "CSS", "JAVASCRIPT", "TYPESCRIPT", "ANGULAR"]
+  object: Array<Iobject> = [
+    {
+      fname: "Jhon",
+      lname: "Doe",
+      email: "jdoe@gmail.com",
+      contact: 123123
+    },
+    {
+      fname: "Rohan",
+      lname: "Panchal",
+      email: "ropo@gmail.com",
+      contact: 123234
+    },
+    {
+      fname: "James",
+      lname: "Bond",
+      email: "jbond@gmail.com",
+      contact: 123234
+    },
+    {
+      fname: "Jun",
+      lname: "Doe",
+      email: "jdoe@gmail.com",
+      contact: 12789
+    }
+  ]
 }
---
+++ src/app/interface.ts
@@ -0,0 +1,6 @@
+export interface Iobject {
+    fname: string;
+    lname: string;
+    email: string;
+    contact: number;
+}
